d5f745532a58e22244ec68bd6f56f20ac47bddfa
qaqkarusa <119190508+qaqkarusa@users.noreply.github.com>
2024-05-03 21:30:06 +0800

    111

---
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Pane root = new Pane();
+        Scene scene1 = new Scene(root, 800, 600);
+
+        Rectangle rect = new Rectangle(50,50,100,100);
+        root.getChildren().add(rect);
+        scene1.setOnMouseMoved(e->{
+            rect.setX(e.getX()- rect.getWidth()/2);
+            rect.setY(e.getY()- rect.getHeight()/2);
+        });
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene1);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
